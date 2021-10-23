65979027b367a44fe23c7582f6f531e11110b438
Apollo23 <85381172+Apollo23SMO@users.noreply.github.com>
2021-10-23 14:58:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -48,6 +48,8 @@ function draw() {
   image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
   image(playerimage,player.position.x,player.position.y,50,180)
 
+  playerArcher.display();
+
   // Title
   fill("#FFFF");
   textAlign("center");
